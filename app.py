dbe3a56c1bfab15eab69a89dd13c9105eefb21ff
EckoTan0804 <ecko0804@gmail.com>
2020-12-29 22:58:19 +0100

    Adjust structure for multi-pages

--- app.py
+++ app.py
@@ -1,40 +1,4 @@
-import os
-
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
-import plotly.express as px
-import pandas as pd
 
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
-df = pd.read_csv(
-    'https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
-
-
+app = dash.Dash(__name__, suppress_callback_exceptions=True)
 server = app.server
-
-app.layout = html.Div([
-    html.H1("TalentWorth"),
-    html.H2('Welcome to TalentWorth'),
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
-    )
-])
-
-
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
-
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
---
+++ index.py
@@ -0,0 +1,89 @@
+import os
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+import pandas as pd
+
+from app import app
+
+
+def build_banner():
+    return html.Div(
+        id="banner",
+        className="banner",
+        children=[
+            html.Div(
+                id="banner-text",
+                children=[
+                    html.H5("TalentWorth"),
+                    html.H6("Make your skills with future trend"),
+                ],
+            ),
+            html.Div(
+                id="banner-logo",
+                children=[
+                    html.Button(
+                        id="learn-more-button", children="LEARN MORE", n_clicks=0
+                    ),
+                ],
+            ),
+        ],
+    )
+
+
+def build_tabs():
+    return html.Div(
+        id="tabs",
+        className="tabs",
+        children=[
+            dcc.Tabs(
+                id="app-tabs",
+                value="tab2",
+                className="custom-tabs",
+                children=[
+                    dcc.Tab(
+                        id="Specs-tab",
+                        label="Specification Settings",
+                        value="tab1",
+                        className="custom-tab",
+                        selected_className="custom-tab--selected",
+                    ),
+                    dcc.Tab(
+                        id="Control-chart-tab",
+                        label="Control Charts Dashboard",
+                        value="tab2",
+                        className="custom-tab",
+                        selected_className="custom-tab--selected",
+                    ),
+                ],
+            )
+        ],
+    )
+
+
+app.layout = html.Div(
+    id="big-app-container",
+    children=[
+        build_banner(),
+        dcc.Interval(
+            id="interval-component",
+            interval=2 * 1000,  # in milliseconds
+            n_intervals=50,  # start at batch 50
+            disabled=True,
+        ),
+        html.Div(
+            id="app-container",
+            children=[
+                build_tabs(),
+                # Main app
+                html.Div(id="app-content"),
+            ],
+        ),
+    ],
+)
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8052)
